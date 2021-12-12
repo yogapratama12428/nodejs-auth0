49e61231b3f9fd75483c6c8681faf0765ac54328
Etornam <sunumacbright@gmail.com>
2021-12-12 08:29:09 +0000

    initial code push

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Welcome to Nodejs with Auth0 tutorials' });
+});
+
+module.exports = router;
